a0af78068bc0626e620b70efd16cd70c6dd83010
satyasathi <83865350+satyasathi@users.noreply.github.com>
2022-10-13 00:00:53 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin >> n;     int arr[n];     for(int i=0;i<n;i++){         cin >> arr[i];     }     int x=arr[0];     for(int i=1;i<n;i++){         x=x ^ arr[i];     }     cout << x ;
    }

---
+++ Single_Number.c
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int x=arr[0];
+    for(int i=1;i<n;i++){
+        x=x ^ arr[i];
+    }
+    cout << x ;
+    
+}
