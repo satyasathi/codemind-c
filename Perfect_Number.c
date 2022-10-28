a8f6beda04e54b89c4a9bd36bffa507e7c3434d8
satyasathi <83865350+satyasathi@users.noreply.github.com>
2022-10-28 21:00:14 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n,s=0;     cin >> n;     for(int i=1;i<n;i++){         if(n%i==0){             s+=i;         }     }     if(s==n){         cout << "True";     }     else{         cout << "False";     } }

---
+++ Perfect_Number.c
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,s=0;
+    cin >> n;
+    for(int i=1;i<n;i++){
+        if(n%i==0){
+            s+=i;
+        }
+    }
+    if(s==n){
+        cout << "True";
+    }
+    else{
+        cout << "False";
+    }
+}
