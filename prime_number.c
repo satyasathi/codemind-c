fab9e5b096ca5cce5ea986202bb31832608175d6
satyasathi <83865350+satyasathi@users.noreply.github.com>
2022-11-02 15:28:45 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin >> n;     int c=0;     for(int i=1;i<=n;i++){         if(n%i==0){             c++;         }     }     if(c==2){         cout << "prime";     }     else{         cout << "not a prime";     } }

---
+++ prime_number.c
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int c=0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            c++;
+        }
+    }
+    if(c==2){
+        cout << "prime";
+    }
+    else{
+        cout << "not a prime";
+    }
+}
