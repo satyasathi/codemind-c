669f62defcdd074ee32196f551c2177c17dcbffe
satyasathi <83865350+satyasathi@users.noreply.github.com>
2022-11-07 11:45:45 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin >> n;     int arr[n];     for(int i=0;i<n;i++){         cin >> arr[i];     }     for(int i=0;i<n;i++){         int c=0;         for(int j=0;j<n;j++){             if(arr[i]==arr[j]){                 c+=1;             }         }         if(c>n/2){             cout << arr[i];             break;         }     } }

---
+++ Majority_Element.c
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for(int i=0;i<n;i++){
+        int c=0;
+        for(int j=0;j<n;j++){
+            if(arr[i]==arr[j]){
+                c+=1;
+            }
+        }
+        if(c>n/2){
+            cout << arr[i];
+            break;
+        }
+    }
+}
