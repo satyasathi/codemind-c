902c423c3bae3322568e866d92c8dff609d27edf
satyasathi <83865350+satyasathi@users.noreply.github.com>
2022-11-07 16:00:38 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     char n;     cin >> n;     if((n=='A') or (n=='E') or (n=='I') or (n=='O') or (n=='U') or (n=='a') or (n=='e') or(n=='i') or (n=='o') or (n=='u')){         cout << "Vowel" ;     }     else{         cout << "Consonant";     }     }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char n;
+    cin >> n;
+    if((n=='A') or (n=='E') or (n=='I') or (n=='O') or (n=='U') or (n=='a') or (n=='e') or(n=='i') or (n=='o') or (n=='u')){
+        cout << "Vowel" ;
+    }
+    else{
+        cout << "Consonant";
+    }
+    }
